d3ee3cca9f56523dc62f945899b8187a38a7e3d3
Shashank Singh <82604413+shashanksingh2002@users.noreply.github.com>
2023-03-31 07:19:20 +0530

    product page added

--- Wizardry DBMS/src/script.js
+++ Wizardry DBMS/src/script.js
@@ -5,8 +5,13 @@ const carouselRightButton = document.querySelector('.right-button');
 const carouselMainImage = document.querySelector('.main-img');
 const carouselContainer = document.querySelector('.carousel-books');
 const loginButton = document.querySelector('.login-button');
+const fiction = document.querySelector('#fiction');
+const bestSeller = document.querySelector('#best-seller');
+const manga = document.querySelector('#manga');
+const author = document.querySelector('#author');
+const genre = document.querySelector('#genre');
 let carouselIdx = 1;
-const carouselImageSlider = ["../assets/carousel1.jpg", "../assets/carousel2.jpg", "../assets/carousel3.jpeg", "../assets/carousel4.jpg", "../assets/carousel5.jpg"];
+const carouselImageSlider = ["../assets/carousel3.jpeg", "../assets/carousel4.jpg", "../assets/carousel5.jpg", "../assets/carousel1.jpg", "../assets/carousel2.jpg"];
 carouselRightButton.addEventListener("click",()=>{
     console.log("right-button clicked");
     if(carouselIdx === carouselImageSlider.length-1) carouselIdx = -1;
@@ -29,6 +34,9 @@ carouselContainer.addEventListener("mouseout",()=>{
     carouselRightButton.style.display = "none";
 });
 
-loginButton.addEventListener('click',()=>{
-    location.href = "login.html";
-});
+loginButton.addEventListener('click',()=>{ location.href = "login.html";});
+fiction.addEventListener('click',()=>{ location.href = "bookinfo.html";});
+manga.addEventListener('click',()=>{ location.href = "bookinfo.html";});
+author.addEventListener("click",()=>{ location.href = "bookinfo.html";});
+genre.addEventListener("click",()=>{ location.href = "bookinfo.html";});
+bestSeller.addEventListener("click",()=>{ location.href = "bookinfo.html";});
